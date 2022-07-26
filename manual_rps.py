31560232e9036d6c07cadbe7d215c4908dffc5fd
NicoMarshall <nico.marshall@btinternet.com>
2022-07-26 12:07:44 +0100

    added get_user_choice function

--- manual_rps.py
+++ manual_rps.py
@@ -7,4 +7,17 @@ def get_computer_choice() :
     computer_choice = random.choice(options)
     return computer_choice
 
- 
+def get_user_choice() :
+    global user_choice 
+    user_choice = input("Your options are rock, paper, or scissors. What is your choice?  ")
+    if user_choice in options:
+        return user_choice
+    else:
+        print("That is not a valid choice.")
+        get_user_choice() 
+    return user_choice
+
+
+
+get_user_choice()
+
